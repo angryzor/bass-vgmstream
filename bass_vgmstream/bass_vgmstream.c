50b53bacf161773a608c47a4f2fcafe3bf3560b6
angryzor <ruben.tytgat@gmail.com>
2014-05-10 15:35:27 +0200

    Removed a lot of things that were redundant.

--- bass_vgmstream/bass_vgmstream.c
+++ bass_vgmstream/bass_vgmstream.c
@@ -3,12 +3,9 @@
 #include <vgmstream.h>
 #include <stdlib.h>
 
-typedef struct VSTREAM {
-	VGMSTREAM* vgmstream;
-	DWORD decode_pos;
-	DWORD stream_length_samples;
-} VSTREAM;
-
+/**
+ * Callback for BASS. Called when it needs more data.
+ */
 DWORD CALLBACK vgmStreamProc(
     HSTREAM handle,
     void *buffer,
@@ -16,31 +13,34 @@ DWORD CALLBACK vgmStreamProc(
     void *user
 )
 {
-	VSTREAM* vs = (VSTREAM*)user;
-	BOOL ended = FALSE;
-	int max_samples = length/sizeof(sample)/vs->vgmstream->channels;
-	int samples_to_do;
+	VGMSTREAM* stream = (VGMSTREAM*)user;                         // We passed the VGMSTREAM as user data.
+	BOOL ended = FALSE;                                           // Used to signal end of stream.
+	int max_samples = length / sizeof(sample) / stream->channels; // Calculate the maximum amount of samples from max buffer size.
+	int samples_to_do;                                            // Will hold the amount of samples to be copied.
 	
-	if (vs->decode_pos + max_samples > vs->stream_length_samples && !vs->vgmstream->loop_flag)
-	{
-		samples_to_do = vs->stream_length_samples - vs->decode_pos;
+	// If this is a looping VGM stream, we handle it as an infinite stream and read out the
+	// next `max_samples` of data.
+	//
+	// Otherwise, we read out at most `max_samples`. If there is less data available,
+	// we consider the stream ended and signal this to BASS
+	if (!stream->loop_flag && stream->current_sample + max_samples > stream->num_samples) {
+		samples_to_do = stream->num_samples - stream->current_sample;
 		ended = TRUE;
 	}
 	else
 		samples_to_do = max_samples;
-	render_vgmstream((sample*)buffer, samples_to_do, vs->vgmstream);
 
-	if(!vs->vgmstream->loop_flag)
-		vs->decode_pos += samples_to_do;
+	// Render the stream.
+	render_vgmstream((sample*)buffer, samples_to_do, stream);
+	// BASS expects you to return the amount of data read in bytes, so multiply by the sample size
+	samples_to_do *= sizeof(sample) * stream->channels;
 
-	samples_to_do *= sizeof(sample) * vs->vgmstream->channels;
-	if(ended)
-	{
+	// If we reached the end of a non-looping VGM stream, we'll check BASS' loop flag.
+	// If it is set, we restart from the beginning. Otherwise, we signal the end of stream
+	// to BASS.
+	if(ended) {
 		if(BASS_ChannelFlags(handle,0,0) & BASS_SAMPLE_LOOP)
-		{
-			reset_vgmstream(vs->vgmstream);
-			vs->decode_pos = 0;
-		}
+			reset_vgmstream(stream);
 		else
 			samples_to_do |= BASS_STREAMPROC_END;
 	}
@@ -48,6 +48,9 @@ DWORD CALLBACK vgmStreamProc(
 	return samples_to_do;
 }
 
+/**
+ * Called when the BASS handle is closed
+ */
 void CALLBACK vgmStreamOnFree(
     HSYNC handle,
     DWORD channel,
@@ -55,27 +58,21 @@ void CALLBACK vgmStreamOnFree(
     void *user
 )
 {
-	VSTREAM* vs = (VSTREAM*)user;
-	close_vgmstream(vs->vgmstream);
-	free(vs);
+	VGMSTREAM* stream = (VGMSTREAM*)user;
+	close_vgmstream(stream);
 }
 
 BASS_VGMSTREAM_API HSTREAM BASS_VGMSTREAM_StreamCreate(const char* file, DWORD flags)
 {
-	VSTREAM* vs;
 	HSTREAM h;
-	VGMSTREAM* v = init_vgmstream(file);
-	if(!v)
+	VGMSTREAM* stream = init_vgmstream(file);
+	if(!stream)
 		return 0;
 
-	vs = (VSTREAM*)malloc(sizeof(VSTREAM));
-	vs->vgmstream = v;
-	vs->decode_pos = 0;
-	vs->stream_length_samples = get_vgmstream_play_samples(0,0,0,v);
-	h = BASS_StreamCreate(v->sample_rate, v->channels, flags, &vgmStreamProc, vs);
+	h = BASS_StreamCreate(stream->sample_rate, stream->channels, flags, &vgmStreamProc, stream);
 	if(!h)
 		return 0;
 
-	BASS_ChannelSetSync(h, BASS_SYNC_FREE|BASS_SYNC_MIXTIME, 0, &vgmStreamOnFree, vs);
+	BASS_ChannelSetSync(h, BASS_SYNC_FREE|BASS_SYNC_MIXTIME, 0, &vgmStreamOnFree, stream);
 	return h;
 }
